4091ecc11a0b3182eb32c9f90dd3bec3b68baaa0
d4ndres <d4ndres@gmail.com>
2022-11-08 16:42:56 -0500

    validations query paramethers

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -28,6 +29,17 @@ def home(): #Path decoration function
 	"PruebaAPI": True
 	} 
 
+#Request and Response Body
 @app.post("/person/new")
 def create_person( person: Person = Body(...)):
 	return person
+
+#Validations: Query Paramethers
+
+#"person/detail?name=Miguel&age=25"
+@app.get('/person/detail')
+def show_person(
+	name: Optional[str] = Query(None, min_length=1, max_length=50),
+	age: int = Query(...)
+	):
+	return { name: age }

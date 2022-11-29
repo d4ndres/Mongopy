a7eb96c9698eb523909b012e1bc2cefaeff98126
d4ndres <d4ndres@gmail.com>
2022-11-29 10:40:51 -0500

    Connect frontend

--- config/db.py
+++ config/db.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
 # MongoDB attributes
-mongodb_uri = 'mongodb+srv://d4:1016@guiac4.3wtun5e.mongodb.net/?retryWrites=true&w=majority'
+mongodb_uri = f'mongodb+srv://d4:1234@learn.0fkccmi.mongodb.net/?retryWrites=true&w=majority'
 port = 8000  
 
 
 conn = MongoClient(mongodb_uri, port)
-db = conn["tiendaOnline"]
+db = conn["usuariosTienda"]
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 #FastAPI
 from fastapi import FastAPI
 from routes.user import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
 	title = "Rest API with FastAPI and MongoDB",
 	description = "this is a simple Rest API using fastapi and MongoDB",
-	version="0.0.1"
+	version="0.0.2"
 	)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user)
